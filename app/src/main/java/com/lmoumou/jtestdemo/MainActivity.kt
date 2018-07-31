b1cb2a3582f6468b5c73747cd4c8bad0e2bbb792
罗世强 <594000722@qq.com>
2018-07-31 10:56:47 +0800

    仓库配置

--- app/src/main/java/com/lmoumou/jtestdemo/MainActivity.kt
+++ app/src/main/java/com/lmoumou/jtestdemo/MainActivity.kt
@@ -2,11 +2,15 @@ package com.lmoumou.jtestdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.lmoumou.test_library.Test
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mTextView.text = Test.test("你好")
     }
 }
---
+++ test_library/src/main/java/com/lmoumou/test_library/Test.kt
@@ -0,0 +1,13 @@
+package com.lmoumou.test_library
+
+/**
+ * Author: Lmoumou
+ * Date: 2018-07-31 10:35
+ * 文件名: Test
+ * 描述:
+ */
+object Test {
+    fun test(str: String): String {
+        return "${str}_test"
+    }
+}
